f05eeaed2efe38f15452dc5deed5797fd611a79f
Angel-Suescun <asuescunr@unal.edu.co>
2026-05-21 10:56:58 -0500

    Crear arquitectura + Spring

---
+++ src/main/java/com/unerp/UnerpApplication.java
@@ -0,0 +1,13 @@
+package com.unerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnerpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unerp/UnerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
