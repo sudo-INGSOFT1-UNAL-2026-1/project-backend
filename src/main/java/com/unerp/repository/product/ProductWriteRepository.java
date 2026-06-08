aba00645152ae223259e3937e4bb9e17f8a07b4d
Juan Pablo Rodríguez Briceño <jrodriguezbr@unal.edu.co>
2026-06-08 03:11:26 -0500

    Files

--- src/main/java/com/unerp/repository/product/ProductWriteRepository.java
+++ src/main/java/com/unerp/repository/product/ProductWriteRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.unerp.domain.product.Product;
 
 public interface ProductWriteRepository extends JpaRepository <Product, Integer> {
+
+    void deleteById(Integer id);
+    void deleteBySupplierId(Integer supplierId);
+    void deleteByBatch(String batch);
 }
---
+++ src/main/java/com/unerp/service/product/ProductDeleteService.java
@@ -0,0 +1,45 @@
+package com.unerp.service.product;
+
+import org.springframework.stereotype.Service;
+
+import com.unerp.repository.product.ProductReadRepository;
+import com.unerp.repository.product.ProductWriteRepository;
+import com.unerp.repository.supplier.SupplierReadRepository;
+
+@Service
+public class ProductDeleteService {
+    
+    private final ProductReadRepository productReadRepository;
+    private final SupplierReadRepository supplierReadRepository;
+    private final ProductWriteRepository productWriteRepository;
+
+    public ProductDeleteService(ProductReadRepository productReadRepository,
+        SupplierReadRepository supplierReadRepository,
+        ProductWriteRepository productWriteRepository) {
+
+        this.productReadRepository = productReadRepository;
+        this.supplierReadRepository = supplierReadRepository;
+        this.productWriteRepository = productWriteRepository;
+    }
+
+    public void deleteProductById(Integer id) {
+        if (!productReadRepository.existsById(id)) {
+            throw new IllegalArgumentException("Product doesn't exist");
+        }
+        productWriteRepository.deleteById(id);
+    }
+
+    public void deleteProductByBatch(String batch) {
+        if (!productReadRepository.existsByBatch(batch)) {
+            throw new IllegalArgumentException("Batch doesn't exist");
+        }
+        productWriteRepository.deleteByBatch(batch);
+    }
+
+    public void deleteProductBySupplierId(Integer supplierId) {
+        if (!supplierReadRepository.existsById(supplierId)) {
+            throw new IllegalArgumentException("Supplier doesn't exist");
+        }
+        productWriteRepository.deleteBySupplierId(supplierId);
+    }
+}
